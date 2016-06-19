d1d181be0aaf45950c89a0d11763b975377e7fe8
Dean Herbert <pe@ppy.sh>
2016-06-19 15:11:46 +0900

    Initial commit.

---
+++ puush-installer/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace puush_installer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelStart = new System.Windows.Forms.Panel();
+            this.panelDownload = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelProgress = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelStart.SuspendLayout();
+            this.panelDownload.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(750, 217);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(217, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(346, 120);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox2.Image = global::puush_installer.Properties.Resources.ShareX_1024;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 528);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(774, 211);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::puush_installer.Properties.Resources.puush_header;
+            this.pictureBox1.InitialImage = global::puush_installer.Properties.Resources.puush_header;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(774, 263);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelStart
+            // 
+            this.panelStart.Controls.Add(this.button1);
+            this.panelStart.Location = new System.Drawing.Point(0, 743);
+            this.panelStart.Name = "panelStart";
+            this.panelStart.Size = new System.Drawing.Size(774, 251);
+            this.panelStart.TabIndex = 4;
+            // 
+            // panelDownload
+            // 
+            this.panelDownload.Controls.Add(this.labelProgress);
+            this.panelDownload.Controls.Add(this.progressBar1);
+            this.panelDownload.Location = new System.Drawing.Point(0, 747);
+            this.panelDownload.Name = "panelDownload";
+            this.panelDownload.Size = new System.Drawing.Size(774, 244);
+            this.panelDownload.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(730, 110);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelProgress.Location = new System.Drawing.Point(0, 190);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(774, 54);
+            this.labelProgress.TabIndex = 1;
+            this.labelProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(774, 996);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelStart);
+            this.Controls.Add(this.panelDownload);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "puush install";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelStart.ResumeLayout(false);
+            this.panelDownload.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panelStart;
+        private System.Windows.Forms.Panel panelDownload;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ puush-installer/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using osu_common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace puush_installer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelStart.Hide();
+            startDownload();
+        }
+
+        private void startDownload()
+        {
+            labelProgress.Text = @"Beginning download...";
+
+            string url = @"https://github.com/ShareX/ShareX/releases/download/v11.0.1/ShareX-11.0.1-setup.exe";
+            FileWebRequest req = new FileWebRequest(@"sharex.exe", url);
+            req.DownloadProgress += downloadProgress;
+            req.Finished += downloadFInished;
+            req.Perform();
+        }
+
+        private void downloadFInished(WebRequest request, Exception e)
+        {
+            Process.Start(@"sharex.exe");
+            Environment.Exit(0);
+        }
+
+        private void downloadProgress(WebRequest request, long current, long total)
+        {
+            Invoke((MethodInvoker)delegate
+            {
+                int percent = (int)((float)current / total * 100);
+                labelProgress.Text = $@"Downloading ({percent}% complete)";
+                progressBar1.Style = ProgressBarStyle.Continuous;
+                progressBar1.Value = percent;
+            });
+        }
+    }
+}
