e25538809c9e19bc61ae3a6f07ef3d9a17e1891c
Jaex <Jaex@users.noreply.github.com>
2016-07-25 16:04:46 +0300

    Removed unused usings

--- puush-installer/MainForm.cs
+++ puush-installer/MainForm.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using puush_installer.ShareX.HelpersLib;
+using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using puush_installer.ShareX.HelpersLib;
 
 namespace puush_installer
 {
